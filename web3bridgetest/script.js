c723088cfb0ac84d939973cb22e5a553b4cb57e3
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-06-15 18:19:43 +0100

    first commit

---
+++ web3bridgetest/script.js
@@ -0,0 +1,32 @@
+const individualDeskSelect = document.getElementById('individual-desk');
+const bookDeskButton = document.getElementById('book-desk');
+const bookingMessage = document.getElementById('booking-message');
+
+const individualDeskPrices = {
+  basic: 10,
+  premium: 15,
+  executive: 20
+};
+
+const teamDeskPrice = 25;
+
+bookDeskButton.addEventListener('click', function() {
+  const deskType = individualDeskSelect.value;
+  const date = document.getElementById('date').value;
+  const startTime = document.getElementById('start-time').value;
+  const endTime = document.getElementById('end-time').value;
+
+  if (!date || !startTime || !endTime) {
+    bookingMessage.textContent = "Please fill in all fields.";
+    return;
+   }
+  
+  let hourPrice;
+  let availDesks = 10; 
+
+  if (deskType === 'team') {
+    hourPrice = teamDeskPrice;
+    availDesks = 5; 
+     } else {
+    hourPrice = individualDeskPrices[deskType];
+     }
